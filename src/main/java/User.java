d6b7249e3582a7e83ad2c968bcfb3b4e1508bacd
zexin-zhang <32444248+zexin-zhang@users.noreply.github.com>
2018-01-15 23:15:38 +0800

    Add files via upload
    更新User Class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,37 @@
 public class User {
 	private String name;
 	private int UserID;
+	Location location = new Location();
+	
+	public User() {
+		this.name = null;
+	}
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public User(double longitude, double latitude) {
+		this.name = null;
+		this.location.setnewlocation(longitude, latitude);
+	}
+	
+	public User(String name, double longitude, double latitude) {
+		this.name = name;
+		this.location.setnewlocation(longitude, latitude);
+	}
+	
+	public void setLocation(double longitude, double latitude) {
+		this.location.setnewlocation(longitude, latitude);
+	}
+	
+	public void setLocation(Location location) {
+		this.location.setnewlocation(location);
+	}
+	
+	public String getLocation() {
+		return this.location.toString();
+	}
 	
 	public void setName(String name) {
 		this.name = name;
@@ -21,6 +52,11 @@ public class User {
 	
 	public String toString() {
 		return "Username: "+this.name+"\n"+
-				"UserID: "+this.UserID;
+				"UserID: "+this.UserID+"\n"+
+				"Location: "+this.getLocation();
+	}
+	
+	public boolean equals(User other) {
+		return this.UserID==other.UserID;
 	}
 }
