e80ad6d232c5424ca8cf2a9fbbee137142e9d83f
zexin-zhang <32444248+zexin-zhang@users.noreply.github.com>
2018-01-17 16:53:11 +0800

    Add files via upload

--- src/test/java/CommentTest.java
+++ src/test/java/CommentTest.java
@@ -12,14 +12,13 @@ public class CommentTest {
 		comment.setCommentID(0);
 		comment.setRestaurantID(1);
 		comment.setUserName("kbh");
-		toString();
 		Comment newComment = new Comment("fucker");
 		
 		assertEquals("hello",comment.getParagraph());
 		assertEquals(1,comment.getRestaurantID());
 		assertEquals("kbh",comment.getUserName());
 		assertEquals("fucker",newComment.getParagraph());
+		assertEquals("User: kbh\n  hello",comment.toString());
+		
 	}
-	
-
 }
