5cf0726678a12d0645e5c50392e6f6124361e76f
zexin-zhang <32444248+zexin-zhang@users.noreply.github.com>
2018-01-17 14:24:11 +0800

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,13 @@ public class Main {
     public static void  main(String args[]) throws IOException, InterruptedException {
         restaurantPack restaurants = new restaurantcsv().main();
         Scanner scanner = new Scanner(System.in);
-        UserPack users = new UserPack();
-        User user = new User();
-        double longitude,latitude;
         String junk;
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         System.out.println("----------------------------------------------------------------------");
         System.out.println("呷崩V1.0       發佈日期：2018/1/19");
-        System.out.println("----------------------------------------------------------------------");
-        System.out.println("初始設定：");
-        System.out.print("你的姓名：");
-        user.setName(scanner.nextLine());
-        System.out.print("你的緯度：");
-        longitude = scanner.nextDouble();
-        junk = scanner.nextLine();
-        System.out.print("你的經度：");
-        latitude = scanner.nextDouble();
-        junk = scanner.nextLine();
-        user.setLocation(longitude,latitude);
+        
 
         restaurants = restaurantcsv(restaurants);
-        users.addUser(user);
-        System.out.println(users.getUser(0).getLocation().toString());
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 
         System.out.println("----------------------------------------------------------------------");
@@ -41,10 +26,10 @@ public class Main {
         switch(choose)
         {
             case 1:
-                Menu menu = new Menu(users,restaurants);
+                Menu menu = new Menu();
                 break;
             case 2:
-                SaleMenu salemenu = new SaleMenu(users,restaurants);
+                SaleMenu salemenu = new SaleMenu(restaurants);
                 break;
         }
 
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Menu {
     UserPack users;
     restaurantPack restaurants;
+    private int userID;
     private int choose;
-    public Menu(UserPack users,restaurantPack restaurants) throws IOException, InterruptedException {
-        this.users = users;
-        this.restaurants = restaurants;
-        menuPrint();
+    public Menu() throws IOException, InterruptedException {
+        this.login();
+        //this.menuPrint();
     }
     public void chooseMenu() throws IOException, InterruptedException {
         switch(choose)
@@ -28,7 +28,7 @@ public class Menu {
         mapSearch mapSearch_activity = new mapSearch(this.users,this.restaurants);
     }
     public void menuPrint() throws IOException, InterruptedException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        //new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         Scanner choose_scan = new Scanner(System.in);
         System.out.println("----------------------------------------------------------------------");
         System.out.println("歡迎使用本軟體，請選擇您所需要的功能");
@@ -44,5 +44,62 @@ public class Menu {
         this.choose=choose_scan.nextInt();
         String junk = choose_scan.nextLine();
         chooseMenu();
+    }
+    public void login() throws IOException, InterruptedException {
+    		Scanner chooses = new Scanner(System.in);
+    		UserPack users = new UserPack();
+    		users = userCsv.read();
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("1.登錄");
+        System.out.println("2.註冊");
+        System.out.println("3.離開");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.print("您的選擇：");
+        this.choose = chooses.nextInt();
+        chooses.nextLine();
+        switch(choose) {
+        		case 1:
+        	        System.out.println("----------------------------------------------------------------------");
+        	        System.out.println("登錄：");
+        	        System.out.print("請輸入您的用戶ID：");
+        	        int ID = chooses.nextInt();
+        	        chooses.nextLine();
+        	        System.out.print("請輸入您的用戶密碼：");
+        	        String pass = chooses.nextLine();
+        	        int i=4;
+        	        while(i>0) {
+        	        		if(users.getUser(ID).getPassword().equals(pass)) {
+        	        			this.userID = ID;
+        	        			this.menuPrint();
+        	        			i=0;
+        	        		} else {
+        	        			System.out.println("密碼錯誤，您還可以嘗試"+i+"次！");
+        	        			i--;
+        	        		}
+        	        }
+        			break;
+        		case 2:
+        			String name;
+        			String pas;
+        	        System.out.println("----------------------------------------------------------------------");
+        	        System.out.println("註冊：");
+        	        System.out.print("請輸入您的用戶名：");
+        	        name = chooses.nextLine();
+        	        	System.out.print("請輸入您的密碼：");
+        	        	pas = chooses.nextLine();
+        	        	System.out.print("請再次確認您的密碼：");
+        	        	if(pas.equals(chooses.nextLine())) {
+        	        		User newUser = new User(name, pas);
+        	        		users.addUser(newUser);
+        	        		System.out.println("恭喜您註冊成功！");
+        	        		System.out.println("您的用戶ID為："+newUser.getUserID()+" (下次登錄時請使用用戶ID登錄)");
+        	        	} else {
+        	        		System.out.println("對不起，兩次密碼不相符！");
+        	        	}
+        			break;
+        		default:
+        			break;
+        }
+        chooses.close();
     }
 }
