bd29a770e89bacf7f67d3db0354cbf70a1a9daaf
fcu-d0511319 <32446725+fcu-d0511319@users.noreply.github.com>
2018-01-17 14:20:01 +0800

    新增
    新增內容

--- src/main/java/type.java
+++ src/main/java/type.java
@@ -9,29 +9,31 @@ public class type
 {
     private restaurantPack foodrestaurant;
     private User username;
-    public void main(restaurantPack restaurants,UserPack users) throws IOException, InterruptedException {
+    public void main(restaurantPack restaurants,UserPack users) throws IOException, InterruptedException
+    {
         Scanner foodtype = new Scanner(System.in);//新增輸入
-        System.out.println("餐廳選項提供:");//選單呈現
+        System.out.println("餐廳類型選項提供:");//選單呈現
         System.out.println("(1)中西式早餐店");
         System.out.println("(2)古早味便當店");
         System.out.println("(3)日式風味店");
-        System.out.println("(4)義式餐廳");
+        System.out.println("(4)義式餐廳店");
         System.out.println("(5)速食店");
         System.out.println("(6)清淡速食店");
         System.out.println("(7)甜食店");
         System.out.println("(8)飲料店");
-        System.out.println("(9)港式餐應");
-        System.out.println("(10)泰式餐廳");
-        System.out.println("(11)越南小吃");
-        System.out.println("(12)客家餐廳");
-        System.out.println("(13)閩式餐廳");
+        System.out.println("(9)港式餐應店");
+        System.out.println("(10)泰式餐廳店");
+        System.out.println("(11)越南小吃店");
+        System.out.println("(12)客家餐廳店");
+        System.out.println("(13)閩式餐廳店");
+        System.out.println("(14)水餃鍋貼店");
         System.out.print("請輸入食物及餐廳種類:");//從這開始進行輸入
         String type = foodtype.nextLine();//輸入的東西傳入type中
         foodrestaurant = restaurants.getRestaurantoftype(type);
 //        username = User.getName();
         //把type當參數傳入restaurants當中的getRestaurantoftype指向foodrestaurant
         int forward_page = 0,last_page = 0;//先宣告並初始化最上面的一頁(forward_page)和最下面的一頁(last_page)
-        if (foodrestaurant.restaurantNumber() < forward_page + 4)
+        if (foodrestaurant.restaurantNumber() < forward_page + 4)//以下為做翻頁的部分跟mapsearch.java雷同，以及呈現一些相關資料
         {
             last_page = foodrestaurant.restaurantNumber();
         }
@@ -47,17 +49,25 @@ public class type
                 {
                     restaurant temp = restaurants.getRestaurant(i);
                     System.out.println(i + "." + temp.getName());
-                    System.out.println("  距離您 " + temp.distance(username.getLocation()) + "公尺");
+                    System.out.println("  距離您大約 " + temp.distance(username.getLocation()) + "公尺(m)");
                     System.out.println("  餐廳類型:" + temp.gettype());
                     System.out.println("  餐廳經緯度:" + temp.getLocation().toString());
+                    if (temp.getOpen())//這個是判斷是否有營業的部分，如果為真就呈現"餐廳營業中的情況"，反之則呈現這個餐廳的營業時間
+                    {
+                        System.out.println(" 餐廳營業中");
+                    }
+                    else
+                    {
+                        System.out.println("  餐廳休息中，營業時間為:"+temp.getRunningtime());
+                    }
                     System.out.println("----------------------------------------------------------------------");
                 }
-                String MenuSelection = foodtype.next();
-                if (MenuSelection.equals("q") || MenuSelection.equals("Q"))
+                String MenuSelection = foodtype.next();//輸入接下來的動作代碼
+                if (MenuSelection.equals("q") || MenuSelection.equals("Q"))//離開
                 {
                     break;
                 }
-                else if (MenuSelection.equals("n") || MenuSelection.equals("N"))
+                else if (MenuSelection.equals("n") || MenuSelection.equals("N"))//下一頁
                 {
                     if (last_page < foodrestaurant.restaurantNumber())
                     {
@@ -77,7 +87,7 @@ public class type
                  }
                  else
                  {
-                        System.out.println("沒有更多了");
+                        System.out.println("沒有更多了!");
                         System.out.println("----------------------------------------------------------------------");
                  }
                 }
@@ -85,10 +95,10 @@ public class type
         }
         else
         {
-            System.out.println("沒有資料");
+            System.out.println("資料已經到底了喔!");
 //            Thread.sleep(1000);
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         }
-    }
 
+    }
 }
