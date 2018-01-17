e0accbca2ce04016ae9a566f053279df09a426f1
zexin-zhang <32444248+zexin-zhang@users.noreply.github.com>
2018-01-17 16:14:36 +0800

    Add files via upload

--- src/main/java/comMenu.java
+++ src/main/java/comMenu.java
@@ -1,41 +1,41 @@
 import java.util.*;
 
 public class comMenu {
-	private int choose;
-	UserPack users;
-	restaurantPack restaurants;
-	CommentList commentList;
+	private int resID;
+	User user;
+	CommentList comments;
 	
-	public comMenu() {
-		commentList = new CommentList();
-	}
-	
-	public comMenu(UserPack users, restaurantPack restaurants) {
-		this.users = users;
-		this.restaurants = restaurants;
+	public comMenu(int resID, User user) {
+		this.resID = resID;
+		this.user = user;
 		this.menuPrint();
 	}
 	
-	public void menuPrint() {
-		Scanner choice = new Scanner(System.in);
-		System.out.println("----------------------------------------------------------------------");
-		System.out.println("請選擇您要查看評論的店家");
-		System.out.println("----------------------------------------------------------------------");
-		System.out.print("您要選擇的店家：");
-		this.choose = choice.nextInt();
-		choice.nextLine();
+	public comMenu(int resID) {
+		this.resID = resID;
+		comments = commentCsv.read();
 		System.out.println("----------------------------------------------------------------------");
 		System.out.println("所有評論：");
-		commentList.getComment(choose);
+		comments.getComment(resID);
+	}
+	
+	public void menuPrint() {
+		comments = commentCsv.read();
+		Scanner choice = new Scanner(System.in);
 		System.out.println("----------------------------------------------------------------------");
-		System.out.print("您是否需要進行評論(按\"1\"為是，按\"0\"為離開)：");
-		this.choose = choice.nextInt();
+		System.out.println("所有評論：");
+		comments.getComment(resID);
+		System.out.println("----------------------------------------------------------------------");
+		System.out.print("命令提示：\"c\"：評論，\"q\"：離開)：");
+		System.out.println("----------------------------------------------------------------------");
+		int choose = choice.nextInt();
 		choice.nextLine();
-		if(this.choose == 1) {
+		if(choose == 1) {
 			System.out.print("請輸入你的評論：");
 			Comment newComment = new Comment(choice.nextLine());
-			newComment.setRestaurantID(this.choose);
-			commentList.addComment(newComment);
+			newComment.setRestaurantID(this.resID);
+			comments.addComment(newComment);
+			commentCsv.write(comments);
 		}
 		choice.close();
 	}
