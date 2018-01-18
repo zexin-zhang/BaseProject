e2911a6090d3efd805764011b5ec0ae2c2e70553
zexin-zhang <32444248+zexin-zhang@users.noreply.github.com>
2018-01-19 04:18:09 +0800

    Add files via upload

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void test() {
+		User user1 = new User();
+		User user2 = new User("kbh");
+		User user3 = new User("gxc","123");
+		
+		assertEquals(user2.getName(),"kbh");
+		assertEquals(user3.getName(),"gxc");
+		assertEquals(user3.getPassword(),"123");
+		
+		user1.setName("kbh");
+		//user1.setPassword();
+		user1.setUserID(0);
+		user1.setLocation(24, 120);
+		user2.setLocation(user1.getLocation());
+		
+		assertEquals(user1.getUserID(), 0);
+		
+	}
+
+}
